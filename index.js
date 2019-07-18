eeaf89956c8f559546f500b70d6f663d3bffd87c
Benjamin Goza <bengoza@gmail.com>
2019-07-18 20:08:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ function nowServing(deliLine){
     return "There is nobody waiting to be served!"
   } else
   return `Currently serving ${deliLine.shift()}.`;
-    //deliLine = deliLine.shift();
-  
 }
 
 nowServing(deliLine);
@@ -27,11 +25,6 @@ function currentLine(deliLine){
   if (deliLine.length === 0) {
     return "The line is currently empty."
   } else {
-    //let i = 0;
-    //for (var i = 0; i < deliLine.length; i++) {
-    //deliLine = ${i+1}. ${deliLine[i]};
-    //return deliLine.slice(0, -1);
-    //}
     deliLine = deliLine.map((c, i) => `${i + 1}. ${c}`).join(', ');
     return `The line is currently: ${deliLine}`;
   }
