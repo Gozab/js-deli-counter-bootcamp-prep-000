3eab27ffcff99afbf1fdc0d989d73ed3933feb6c
Benjamin Goza <bengoza@gmail.com>
2019-07-18 14:23:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 //probably want to return the array and just slice off the last position
 function takeANumber(line, name) {
   //var line = katzDeli.length;
-  return `"Welcome, ${name}. You are number ${katzDeliLine.length} in line."`
+  return `"Welcome, ${name}. You are number ${line} in line."`
 }
 
 takeANumber();
