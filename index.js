ef98bd5da36f256dc0909bc63985ee6eb31ae67f
Benjamin Goza <bengoza@gmail.com>
2019-07-18 18:40:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function currentLine(deliLine){
   if (deliLine.length === 0) {
     return "The line is currently empty."
   } else {
-    let i = 0;
+    let i = 1;
     return `The line is currently: ${i}. ${deliLine[i]}. `
     i++;
   }
