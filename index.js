e83efdca7c3b46946cbc1273d282d6b5a5515ac9
Benjamin Goza <bengoza@gmail.com>
2019-07-18 17:05:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function takeANumber(katzDeli, name) {
   let i = i + 1;
   //i++;
   katzDeli.push(i);
-  return `"Welcome, ${name}. You are number ${katzDeli.slice(-1)} in line."`
+  return `"Welcome, ${name}. You are number ${katzDeli.length} in line."`
 }
 
 takeANumber(katzDeli, name);
