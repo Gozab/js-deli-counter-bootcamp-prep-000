3ccf9a0f5a1260f2f66c80c1f6dc277a1a77cf40
Benjamin Goza <bengoza@gmail.com>
2019-07-18 14:49:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var line = katzDeli.length
+var line = katzDeli.length;
 //probably want to return the array and just slice off the last position
 function takeANumber(line, name) {
  // var line = katzDeli.length;
