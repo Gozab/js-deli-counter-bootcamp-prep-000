dec511a76b8a3a698c0a33089b77147fb067b07d
Benjamin Goza <bengoza@gmail.com>
2019-07-18 17:39:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function nowServing(deliLine){
     return "There is nobody waiting to be served!"
   } else
   return `"Currently serving ${deliLine[0]}."`;
-  deliLine.shift;
+  deliLine.shift();
 
 }
 
